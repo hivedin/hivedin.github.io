d6a19de9112dde01144de7226e309db3b08ee668
Sujit <sjoshi1@saic.edu>
2020-09-04 16:42:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,13 +8,26 @@ function draw() {
     background('#EFE8DE');
     frameRate(120);
     noStroke();
+    // fill(255,206,71,255);
+    // ellipse(mouseX, mouseY, 120, 120);
     fill(255,206,71,255);
-    ellipse(mouseX, mouseY, 120, 120);
-
-    fill(255,206,71,255);
-    ellipse(mouseX, mouseY, 120, 120);
+    push();
+    polygon(mouseX, mouseY, 120, 6);
+    pop();
 }
 
 function windowResized(){
     resizeCanvas(windowWidth,windowHeight);
 }
+
+function polygon(x, y, radius, npoints) {
+    let angle = TWO_PI / npoints;
+    beginShape();
+    for (let a = 0; a < TWO_PI; a += angle) {
+      let sx = x + cos(a) * radius;
+      let sy = y + sin(a) * radius;
+      vertex(sx, sy);
+    }
+    endShape(CLOSE);
+  }
+  
