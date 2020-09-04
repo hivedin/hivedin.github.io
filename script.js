71a35c342563d83aa1913b7514a2bdaed3dfdbb8
Sujit <sjoshi1@saic.edu>
2020-09-04 15:34:38 +0530

    commit

--- script.js
+++ script.js
@@ -4,10 +4,12 @@ function setup() {
     canvas = createCanvas(windowWidth, windowHeight);
     canvas.position(0, 0);
     canvas.style('z-index', -1);
+    
 }
 
 function draw() {
     background('#EFE8DE');
+    frameRate(120);
     noStroke();
     fill(255,206,71,255);
     ellipse(mouseX, mouseY, 120, 120);
