f43a3c067c24946e364df30773d463445140abc9
Sujit <sjoshi1@saic.edu>
2020-09-05 10:48:08 +0530

    built entirely new page

--- script.js
+++ script.js
@@ -1,37 +1,38 @@
 function setup() {
-    canvas = createCanvas(windowWidth, windowHeight);
-    canvas.position(0, 0);
-    canvas.style('z-index', '-1');
+    //let vh = windowHeight/100;
+    //let vw = windowWidth/100;
+    let vh = 330;
+    let vw = 330;
+    canvas = createCanvas(40 * vw, 30*vh);
+    canvas.parent("sketchContainer");
 }
 
 function draw() {
-    background('#EFE8DE');
-    frameRate(120);
-    noFill();
-    strokeWeight(8);
-    stroke(255,206,71,255);
+    // background('#EFE8DE');
+    background(0);
+    frameRate(60);
 
     endFunction();
 }
 
-function windowResized(){
-    resizeCanvas(windowWidth,windowHeight);
-}
+// function windowResized() {
+//     resizeCanvas(windowWidth, windowHeight);
+// }
 
 function hexagon(x, y, radius) {
     let angle = TWO_PI / 6;
     beginShape();
     for (let a = 0; a < TWO_PI; a += angle) {
-      let sx = x + cos(a) * radius;
-      let sy = y + sin(a) * radius;
-      vertex(sx, sy);
+        let sx = x + cos(a) * radius;
+        let sy = y + sin(a) * radius;
+        vertex(sx, sy);
     }
     endShape(CLOSE);
-  }
-
-function endFunction(){
-    noStroke();
-    fill(255,206,71,255);
-    ellipse(mouseX, mouseY, 120, 120);
 }
-  
+
+function endFunction() {
+    noStroke();
+    fill(255, 206, 71, 255);
+    ellipse(mouseX, mouseY, 120,120);
+}
+
